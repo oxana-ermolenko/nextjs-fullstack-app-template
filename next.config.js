1882307184281c549b23ae51a726a3d9c7b8ab0d
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2022-08-17 00:08:26 -0700

    feat: create CatCard

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['i.pravatar.cc'],
+  },
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+    // localeDetection: false,
+  },
 };
 
 module.exports = nextConfig;
